39ab2d0375b3cfae45f402a0c2b26c68d15b3707
HadiPr <hadipeysepar79@gmail.com>
2021-11-25 22:50:17 +0330

    add cors header and 404

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 
 const helmet = require('helmet')
+const cors = require('cors')
 const authMiddleware = require('./middlewares/auth')
 const { connectDb } = require('./helpers.js')
 const wordRoutes = require('./controllers/wordControllers')
 const quizRoutes = require('./controllers/quizControllers')
 const userRoutes = require('./controllers/userController')
 
+app.use(cors())
 app.use(express.json())
 app.use(helmet())
 
@@ -18,6 +20,8 @@ router.use('/quiz',authMiddleware, quizRoutes)
 router.use('/auth', userRoutes)
 
 app.use('/api', router)
+
+app.use((req, res)=> res.status(404).json({errorMessage: 'page not found'}))
 
 app.use((error, req, res, next) => {
   res.status(500).json({ errorMessage: error.message })
