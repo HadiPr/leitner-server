d53598c5f6d017e2aecd5b005ac18b673f96f70b
HadiPr <hadipeysepar79@gmail.com>
2022-01-22 20:40:19 +0330

    try set answer api

--- controllers/quizControllers.js
+++ controllers/quizControllers.js
@@ -14,8 +14,8 @@ router.get('/', async (req, res, next) => {
           const { quiz_per_day, countof_correct_answers_to_pass_word: maxTrueAnswers } = setting
           const today = new Date(req.query.date).toISOString().slice(0, 10)
           const runningQuiz = quiz.find(item => !item.is_done)?._doc
-          if(quiz.length === quiz_per_day && !runningQuiz){
-               res.json({message: 'You\'re finished your quizes today!'})
+          if (quiz.length === quiz_per_day && !runningQuiz) {
+               res.json({ message: 'You\'re finished your quizes today!' })
           }
           if (!runningQuiz) {
                //generate quiz
@@ -30,9 +30,9 @@ router.get('/', async (req, res, next) => {
                          word: word._id
                     }))
                })
-               newQuiz = await newQuiz.save() 
+               newQuiz = await newQuiz.save()
 
-               res.json({data: {...newQuiz._doc, words: quizWords.map(word => word._doc)}})
+               res.json({ data: { ...newQuiz._doc, words: quizWords.map(word => word._doc) } })
           } else {
                runningQuiz.words = runningQuiz.words.filter(({ is_answered }) => !is_answered)
                runningQuiz.words = runningQuiz.words.map(({ _doc }) => _doc.word._doc)
@@ -42,11 +42,12 @@ router.get('/', async (req, res, next) => {
           console.log(error)
      }
 })
-router.patch('/answer/:id', (req, res, next) => {
-     const { id } = req.params
-     const today = new Date(req.query.date).toISOString().slice(0, 10)
+router.patch('/answer', async (req, res, next) => {
+     const { quiz_id, word_id } = req.body;
+     if (!quiz_id || !word_id) {
+          res.status(422).json({ errorMessage: 'quiz_id or word_id is missed' })
+     }
      const { answer } = req.query
-     if (!id) res.status(400).json({ errorMessage: 'id not found' })
      if (!answer) res.status(400).json({ errorMessage: 'specify answer in query => ?answer=true' })
      let nameOfKey;
      switch (answer) {
@@ -57,22 +58,48 @@ router.patch('/answer/:id', (req, res, next) => {
                nameOfKey = 'countOfWrongAnswers'
                break;
      }
-     Word.findOne({ user_id: req.user_id, _id: id }).then(word => {
-          // const nthQuizInDay = +word.updatedDate.split('--')[1]
-          word[nameOfKey] += 1
-          if (today === word.updated_date) {
-               word.daily_updates += 1
-          } else {
-               word.daily_updates = 1
-          }
-          word.updated_date = `${new Date().toISOString().slice(0, 10)}`
+     const [quiz, word] = await Promise.all([
+          Quiz.findOne({ $and: [{ _id: quiz_id }, {'words.word': {$elemMatch: [word_id]}}] }),
+          // Quiz.findOne({ $and: [{ _id: quiz_id }, {'words.word': word_id}] }),
+          Word.findOne({ user_id: req.user_id, _id: word_id }),
+     ])
+     // quiz.words[0] = { ...quiz.words[0], is_answered: true }
+     console.log('=>', quiz)
+     // => {
+     //      _id: new ObjectId("61ec38e8aa15a208403055e5"),
+     //      user_id: new ObjectId("61dc525d0f4044d2dd604947"),
+     //      date: 2022-01-22T00:00:00.000Z,
+     //      is_done: false,
+     //      words: [
+     //        {
+     //          word: new ObjectId("61ddadc34adb428db22cfeee"),
+     //          is_answered: false,
+     //          _id: new ObjectId("61ec38e8aa15a208403055e6")
+     //        },
+     //        {
+     //          word: new ObjectId("61ddad374adb428db22cfe81"),
+     //          is_answered: false,
+     //          _id: new ObjectId("61ec38e8aa15a208403055e7")
+     //        }
+     //      ],
+     //      __v: 0
+     //    }
+     // word[nameOfKey] += 1
+     // word.updated_date = `${new Date().toISOString().slice(0, 10)}`
 
-          return word.save()
-     })
-          .then((r) => {
-               res.json({ data: { success: true, _id: r._id } })
-          })
-          .catch(err => next(err))
+     try {
+          // const [, savedWord] = await Promise.all([
+          //      quiz.save(),
+          //      word.save(),
+          // ])
+          // res.json({ data: { success: true, _id: savedWord._id } })
+     } catch (error) {
+          console.log('=>', error.message)
+     }
+     // .then((r) => {
+     //      console.log('=>', r)
+     // })
+     // .catch(err => next(err))
 })
 
 
